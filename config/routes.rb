5864a36a06af7621e9199873102527b4715ccd4b
julien kiang <kiang.jn@gmail.com>
2022-02-24 12:55:18 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get "/dashboard", to: "pages#dashboard", as: "dashboard"
+
   resources :pokemons do
     resources :rentals, only: [:create, :show]
   end
