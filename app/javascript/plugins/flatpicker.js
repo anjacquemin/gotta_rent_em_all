78cc58bbc58fbe7e922ebb0adb280913971c5ad5
Anthony Jacquemin <an.jacquemin@gmail.com>
2022-02-24 15:28:57 +0100

    add availaibility on planning

--- app/javascript/plugins/flatpicker.js
+++ app/javascript/plugins/flatpicker.js
@@ -1,9 +1,20 @@
 // app/javascript/plugins/flatpickr.js
-// import flatpickr from "flatpickr";
-// import 'flatpickr/dist/flatpickr.min.css'
+import flatpickr from "flatpickr";
+import 'flatpickr/dist/flatpickr.min.css'
+// import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
-// const initFlatpickr = () => {
-//   flatpickr(".datepicker", {});
-// }
 
-// export { initFlatpickr };
+const initFlatpickr = () => {
+  flatpickr(".datepicker", {
+    altInput: true
+  });
+
+  // To keep in case we want ro try again the range directly
+
+  // flatpickr("#range_start", {
+  //   altInput: true,
+  //   plugins: [new rangePlugin({ input: "#range_end"})]
+  // });
+}
+
+export { initFlatpickr };
