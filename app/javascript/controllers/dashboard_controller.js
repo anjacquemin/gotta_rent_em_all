bdfd4dc533d5cbd2ea1918d6051e40e8b47caf47
julien kiang <kiang.jn@gmail.com>
2022-02-25 12:38:49 +0100

    ok

--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -15,10 +15,23 @@ export default class extends Controller {
     const tab1 = document.querySelector("#current-bookings");
     const tab2 = document.querySelector("#future-bookings");
     const tab3 = document.querySelector("#past-bookings");
+
+    const button1 = document.querySelector("#button1");
+    const button2 = document.querySelector("#button2");
+    const button3 = document.querySelector("#button3");
+    console.log(target)
+
     // console.log(tab)
     tab1.classList.add("tabcontent")
     tab2.classList.add("tabcontent")
     tab3.classList.add("tabcontent")
     tab.classList.remove("tabcontent")
+
+    button1.classList.remove("tabgrey")
+    button2.classList.remove("tabgrey")
+    button3.classList.remove("tabgrey")
+    console.log(event.currentTarget)
+    event.currentTarget.classList.add("tabgrey")
+
   }
 }
